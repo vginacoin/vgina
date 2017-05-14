59ba2d66abb64535fcba400df2cae871e364391b
vginacoin <vginacoin@gmail.com>
2017-05-14 10:41:51 -0400

    First Commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define vgina-purse message queue name
+#define BITCOINURI_QUEUE_NAME "VGINAURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
